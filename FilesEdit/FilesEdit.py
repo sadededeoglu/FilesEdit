3fc66041250e195dcd78f163e8b1ceef689fffac
sadiye Buyukcukurlu <sadiyebuyukcukurlu@gmail.com>
2020-11-09 15:36:56 +0300

    over

--- FilesEdit/FilesEdit.py
+++ FilesEdit/FilesEdit.py
@@ -1,19 +1,18 @@
 import os
 
-Source = os.getcwd() #Getcwd:İçinde bulunduğumuz klasörü döndüren bir fonksiyondur.
-Files = os.listdir(Source) #Listdir:Bir dizin içindeki dosya ve klasörleri listeleme imkanı verir. 
+Source = os.getcwd()
+#Getcwd:İçinde bulunduğumuz klasörü döndüren bir fonksiyondur.
+for File in os.listdir(Source):
+#Listdir:Bir dizin içindeki dosya ve klasörleri listeleme imkanı verir. 
+	if File.endswith(".txt"):
+		Target_File = Source + "/txt_Files/"
 
-for File in Files:
-	if File.endswith(".txt"): #dosyanın uzantısı .txt ile bitiyor ise bulunması gereken klasör e taşır.
-		Target_File = Source + "/txt_Files/";
-
-		if not os.path.isdir(Target_File): #Klasör yok ise olusturur.
+		if not os.path.isdir(Target_File):
 			os.mkdir(Target_File)
-		
-			os.rename(Source + "/" + File, Target_File + File);
+		os.rename(Source + "/" + File, Target_File + File);
 
 	elif File.endswith(".ppt"):
-		Target_File = Source + "/ppt_Files/";
+		Target_File = Source + "/ppt_Files/"
 
 		if not os.path.isdir(Target_File):
 			os.mkdir(Target_File)
@@ -23,8 +22,9 @@ for File in Files:
 	elif File.endswith(".docx"):
 #Bir karakter dizisinin hangi karakter dizisi ile bittiğini sorgulayabilir.
 		Target_File = Source + "/docx_Files/";
+#dosyanın uzantısı .txt ile bitiyor ise bulunması gereken klasör e taşır.
 		if not os.path.isdir(Target_File):
-#İsdir:Parametre olarak verilen öğenin bir dizin olup olmadığını sorgular
+#İsdir:Parametre olarak verilen öğenin bir dizin olup olmadığını sorgular.
 			os.mkdir(Target_File)
 #Mkdir:Yeni dizinler oluşturabilmemizi sağlar.
 		os.rename(Source + "/" + File, Target_File + File);
